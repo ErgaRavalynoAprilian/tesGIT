75a82b991a754c7cdc4e735af9cb7eaa524c5e76
ErgaRavalynoAprilian <50563445+ErgaRavalynoAprilian@users.noreply.github.com>
2019-05-13 10:18:14 +0700

    menubuku.php

---
+++ buku.php
@@ -0,0 +1,40 @@
+<?php
+
+class buku {
+	// properties
+	public $judulbuku;
+	public $pengarang ="yusuf";
+	public $penerbit;
+	public $tahunterbit;
+	public $cetakan;
+	
+	 // method = hello
+	public function buku()
+	{
+		return "hasil buku";
+	}
+}
+
+//Membuat instance
+$rpl = new buku ();
+
+// Get values
+echo $rpl -> buku();
+echo "<br />";
+echo "<hr />";
+
+// Get values
+echo $rpl -> judulbuku = "RPL";
+echo "<br />";
+echo $rpl -> pengarang; 
+echo "<br />";
+echo $rpl -> penerbit = "Gramedia";
+echo "<br />";
+echo $rpl -> tahunterbit = "2019";
+echo"<br />";
+echo $rpl -> cetakan ="New";
+echo "<br/>";
+
+?>
+?
+?
